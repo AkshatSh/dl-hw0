86d681970f8cadde49eceaee481f004414e4a2ec
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-10-04 12:43:43 -0700

    Finished 2.1

--- src/activations.c
+++ src/activations.c
@@ -15,17 +15,21 @@ void activate_matrix(matrix m, ACTIVATION a)
             double x = m.data[i*m.cols + j];
             if(a == LOGISTIC){
                 // TODO
+                m.data[i*m.cols + j] = 1.0 / (1.0 + exp(-x));
             } else if (a == RELU){
-                // TODO
+               m.data[i*m.cols + j] = x > 0 ? x : 0;
             } else if (a == LRELU){
-                // TODO
+                m.data[i*m.cols + j] = x > 0 ? x : 0.1 * x;
             } else if (a == SOFTMAX){
-                // TODO
+                 m.data[i*m.cols + j] = exp(x);
             }
             sum += m.data[i*m.cols + j];
         }
         if (a == SOFTMAX) {
             // TODO: have to normalize by sum if we are using SOFTMAX
+            for(j = 0; j < m.cols; j++) {
+                m.data[i*m.cols + j] /= sum;
+            }
         }
     }
 }
